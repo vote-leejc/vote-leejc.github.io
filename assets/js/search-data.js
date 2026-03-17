2ad37190a87b8ac2e5f34b2b69c56a5432afc038
vote-leejc <bumsogeosa@gmail.com>
2026-03-17 10:47:17 +0000

    Deploying to gh-pages from @ vote-leejc/vote-leejc.github.io@a1eed1afcddb0e83185b5ff1f4d12576bccfbc67 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -532,7 +532,7 @@ ninja.data = [{
         title: 'RSS Feed',
         section: 'Socials',
         handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("./feed.xml", "_blank");
         },
       },{
         id: 'social-scholar',
